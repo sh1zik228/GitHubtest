e06114ec055c3853c38e483a190720ade067d5af
sh1zik228 <161465419+sh1zik228@users.noreply.github.com>
2024-02-27 20:40:36 +0000

    Added axis function

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 import math
 
-def axis(array):
-    min_val = min(array)
-    max_val = max(array)
+n = int(input("Введіть кількість елементів у послідовності: "))
+array = []
 
-    axis_length = max_val - min_val
+print("Введіть елементи послідовності:")
+for i in range(n):
+  element = float(input())
+  array.append(element)
 
-    return axis_length
+min_val = min(array)
+max_val = max(array)
 
-try:
-    n = int(input("Введіть кількість елементів у послідовності: "))
-    if n > 0:
-        array = []
+axis_length = max_val - min_val
 
-        print("Введіть елементи послідовності:")
-        for i in range(n):
-            element = float(input())
-            array.append(element)
-
-        length = axis(array)
-
-        print("Найменша довжина числової осі:", length)
-    else:
-        print("Введіть правильне значення")
-except ValueError:
-    print("Введіть правильне значення")
+print("Довжина осі:", axis_length)
